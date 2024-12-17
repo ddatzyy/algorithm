eefdbc1cada56b56509a4de380da667fb2caec40
ddatzyy <ddatzyy@gmail.com>
2024-12-17 20:35:00 +0900

    good

--- 그래프/block_cut_tree.cpp
+++ 그래프/block_cut_tree.cpp
@@ -6,20 +6,13 @@ struct block_cut_tree : public biconnected_component {
     {
         biconnected_component::init();
         BN = biconnected_component::bcc.size();
-        blk.resize(BN + N); // cut(i >= BN) -> blk(i < BN) -> cut(i >= BN) ...
+        blk.resize(BN + N);  // cut(i >= BN) -> blk(i < BN) -> cut(i >= BN) ...
         vector<bool> chk(N);
-        for (int i = 0; i < BN; i++) {
-            for (auto id : biconnected_component::bcc[i]) {
-                for (auto ne : edg[id])
-                    if (biconnected_component::is_cut_v[ne])
-                        chk[ne] = 1;
+        for (int i = 0; i < BN; i++)
+            for (auto ne : biconnected_component::bcc_vertex[i]) {
+                if (biconnected_component::is_cut_v[ne])
+                    chk[ne] = 0, blk[ne + BN].push_back(i), blk[i].push_back(ne + BN);
             }
-            for (auto id : biconnected_component::bcc[i]) {
-                for (auto ne : edg[id])
-                    if (biconnected_component::is_cut_v[ne])
-                        chk[ne] = 0, blk[ne + BN].push_back(i), blk[i].push_back(ne + BN);
-            }
-        }
     }
 
     int BN, N;
