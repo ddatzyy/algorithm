f1b769ae92aac716c6f1f88c9969fa5d9cf71c9c
ddatzyy <ddatzyy@gmail.com>
2025-01-26 23:15:20 +0900

    scc

--- 구현체/time_converter.cpp
+++ 구현체/time_converter.cpp
@@ -1,7 +1,7 @@
 namespace time_converter {
 
 // 0000-01-01 = day 1
-int one_year = 365;
+constexpr int one_year = 365;
 // 0 : not leap year, 1 : leap year
 bool leap_year = true;
 vector<vector<int>> month({{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
@@ -58,15 +58,52 @@ array<int, 3> day_to_year(int d)
 }
 
 // hour : minute : second
-int one_hour = 60, one_minute = 60;
-int time_to_second(int h, int m, int s)
-{
-    assert(m >= 0 && m < one_hour && s >= 0 && s < one_minute);
-    return (h * one_hour + m) * one_minute + s;
-}
-array<int, 3> second_to_time(int s)
-{
-    assert(s >= 0);
-    return {s / (one_minute * one_hour), s % (one_minute * one_hour) / one_hour, s % one_minute};
-}
+constexpr int one_hour = 60, one_minute = 60;
+struct time_od {
+    time_od() : H(0), M(0), S(0) {}
+    time_od(int h, int m, int s) : H(h), M(m), S(s) { normalize(); }
+    time_od(array<int, 3> va) : H(va[0]), M(va[1]), S(va[2]) { normalize(); }
+    static int time_to_s(int h, int m, int s)
+    {
+        assert(m >= 0 && m < one_hour && s >= 0 && s < one_minute);
+        return (h * one_hour + m) * one_minute + s;
+    }
+    static int time_to_s(time_od va)
+    {
+        assert(va.M >= 0 && va.M < one_hour && va.S >= 0 && va.S < one_minute);
+        return (va.H * one_hour + va.M) * one_minute + va.S;
+    }
+    static int time_to_s(array<int, 3> va)
+    {
+        assert(va[1] >= 0 && va[1] < one_hour && va[2] >= 0 && va[2] < one_minute);
+        return (va[0] * one_hour + va[1]) * one_minute + va[2];
+    }
+
+    static time_od s_to_time(int s)
+    {
+        assert(s >= 0);
+        return {s / (one_minute * one_hour), s % (one_minute * one_hour) / one_hour, s % one_minute};
+    }
+
+    time_od &operator+=(const time_od &va) { return *this = s_to_time(time_to_s(*this) + time_to_s(va)); }
+    time_od &operator-=(const time_od &va) { return *this = s_to_time(time_to_s(*this) - time_to_s(va)); }
+    time_od operator+(const time_od &va) const { return time_od(*this) += va; }
+    time_od operator-(const time_od &va) const { return time_od(*this) -= va; }
+    bool operator==(const time_od &va) const { return array<int, 3>({H, M, S}) == array<int, 3>({va.H, va.M, va.S}); }
+    bool operator!=(const time_od &va) const { return !(*this == va); }
+
+    friend ostream &operator<<(ostream &os, const time_od &va)
+    {
+        return os << "(" << va.H << ":" << va.M << ":" << va.S << ")";
+    }
+
+    void normalize()
+    {
+        M += S / one_minute, S %= one_minute;
+        H += M / one_hour, M %= one_hour;
+    }
+
+    int H, M, S;
+};
+
 };  // namespace time_converter
