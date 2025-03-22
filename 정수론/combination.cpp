2ff48726ed75ea3d931d7b2c9f87ad19b87188b5
ddatzyy <ddatzyy@gmail.com>
2025-03-22 23:45:24 +0900

    comb

--- 정수론/combination.cpp
+++ 정수론/combination.cpp
@@ -1,40 +1,37 @@
+
+template <typename comb_t>
 struct combination {
-    combination(int64_t _N, int64_t _md) : N(_N), md(_md)
+    combination(int _N) : N(_N)
     {
         fac.resize(N);
         ifac.resize(N);
         fac[0] = ifac[0] = 1;
         for (int i = 1; i < fac.size(); i++)
-            fac[i] = fac[i - 1] * i % md;
-        ifac.back() = power(fac.back(), md - 2);
+            fac[i] = fac[i - 1] * i;
+        ifac.back() = 1 / fac.back();
         for (int i = ifac.size() - 2; i > 0; i--)
-            ifac[i] = ifac[i + 1] * (i + 1) % md;
+            ifac[i] = ifac[i + 1] * (i + 1);
     }
-    int64_t power(int64_t a, int64_t b)
+    comb_t power(int64_t a, int64_t b)
     {
-        int64_t ret = 1;
+        comb_t ret = 1;
         while (b) {
-            if (b & 1) ret = ret * a % md;
-            a = a * a % md, b >>= 1;
+            if (b & 1) ret = ret * a;
+            a = a * a, b >>= 1;
         }
         return ret;
     }
-    int64_t inverse(int64_t a)
-    {
-        if (a > 0 && a < N) return ifac[a] * fac[a - 1] % md;
-        return power(a, md - 2);
-    }
-    int64_t nCr(int64_t a, int64_t b)
+    comb_t nCr(int a, int b)
     {
         assert(max(a, b) < N);
         if (a < b) return 0;
-        return fac[a] * ifac[b] % md * ifac[a - b] % md;
+        return fac[a] * ifac[b] * ifac[a - b];
     }
-    int64_t nHr(int64_t a, int64_t b) { return nCr(a + b - 1, b); }
-    int64_t factorial(int64_t a) { return fac[a]; };
-    int64_t inverse_factorial(int64_t a) { return ifac[a]; }
+    comb_t nHr(int a, int b) { return nCr(a + b - 1, b); }
+    comb_t factorial(int a) { return fac[a]; };
+    comb_t inverse_factorial(int a) { return ifac[a]; }
 
    private:
-    int64_t N, md;
-    vector<int64_t> fac, ifac;
+    int N;
+    vector<comb_t> fac, ifac;
 };
